08eb75c2c9ca848bc212e1c04de0228487fc676d
Gleisson de Assis <gleisson@SysMaps-MacBook-Pro.local>
2019-10-28 21:38:00 -0300

    updating contract

--- src/Config.js
+++ src/Config.js
@@ -1,17 +1,17 @@
-export default {
+/*export default {
   brltMoneyContractAddress: "0xDAc81796e245A7AE73e63552770A2d7F2B10D903",
   smcContractAddress: "0x4d6a3c55AAC2e20afbC54b382e36ce215A7F06Cc",
   rpcUrl: "http://127.0.0.1:7545",
   wsRPCUrl: "ws://127.0.0.1:7545",
   gasLimit: 400000,
   gasPrice: 20,
-}
+}*/
 
-/*export default {
-  brltMoneyContractAddress: "0xbC1e037C02a542e8aBeF8C1C49c4997498a9A008",
-  smcContractAddress: "0x1f165F8450630f24edcB2CA791f2D1ad8ABFAcDa",
+export default {
+  brltMoneyContractAddress: "0x8D19D671F39cA969141860AB03e371CbDE5E5F4B",
+  smcContractAddress: "0xb4135319a753829a5192A2753b88551e4Aa0Fe2D",
   rpcUrl: "https://ropsten.infura.io/v3/971cb825f16b4607acc7349a1ca350ab",
   wsRPCUrl: "wss://ropsten.infura.io/ws",
   gasLimit: 400000,
   gasPrice: 50,
-}*/
+}
